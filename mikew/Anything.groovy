7cf147c9179c7af3a2ad0cc0a29adcf66806e98a
leadtrip <mike.wood@weareact.com>
2022-01-24 14:38:03 +0000

    Playing

--- mikew/Anything.groovy
+++ mikew/Anything.groovy
@@ -1,49 +1,6 @@
-import groovy.sql.Sql
+assert 2 == Math.abs(2)
+assert 2 == Math.abs(-2)
 
-class Test {
-
-    private static final String PROJECT_INFO_FOR_USER ="select PROJECT_ID as PROJECT_NO, NAME as PROJECT_NAME from PROJECT_DIM where PROJECT_ID IN ( "
-
-    private getProjectList( caseResult ) {
-        def query = new StringBuffer( PROJECT_INFO_FOR_USER )
-        def binds = []
-        caseResult.each { projno ->
-            if (query.length() == 0) {
-                query.append '?,'
-                binds << projno.project_no
-            } else {
-                if (query.indexOf(projno.project_no) == -1) {
-                    query.append '?,'
-                    binds << projno.project_no
-                }
-            }
-        }
-
-        query.deleteCharAt( query.length() - 1 )
-        query.append( ' )' )
-        [query: query.toString(), binds: binds]
-    }
-
-    private process() {
-        def db = [url: "jdbc:oracle:thin:@x.xx.xx.xx:1521:ORCL",
-                  user: 'xxxx',
-                  password: 'xxxx',
-                  driver: 'oracle.jdbc.pool.OracleDataSource']
-        def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
-        println "DB connection ready"
-
-        def caseResult = [['project_no':'x-xxxx', 'case_nos':['12344'], 'updated_on':'1485335172'], ['project_no':'y-yyyy', 'case_nos':['56789'], 'updated_on':1490359241]]
-        def projectList = "x-xxxx"
-        def params = getProjectList(caseResult)
-
-        def result = sql.rows( params.query, params.binds ).collect {          // If I replace params with projectList then 'result' is assigned a row from oracle database
-            it as Map
-        }
-        println result
-    }
-
-    public static void main(String[] args) {
-        Test t = new Test()
-        t.process()
-    }
-}
+assert -1f == Math.signum( -23 )
+assert 0f == Math.signum( 0 )
+assert 1f == Math.signum( 3 )
