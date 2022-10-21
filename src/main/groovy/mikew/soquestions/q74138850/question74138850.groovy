8fbc1c4c09df47bc1d9adb8c1a0582b4b66e5102
leadtrip <woodm@objectcomputing.com>
2022-10-21 14:13:55 +0100

    Playing

--- src/main/groovy/mikew/soquestions/q74138850/question74138850.groovy
+++ src/main/groovy/mikew/soquestions/q74138850/question74138850.groovy
@@ -4,6 +4,28 @@ import groovy.xml.MarkupBuilder
 import groovy.xml.XmlSlurper
 import groovy.xml.XmlUtil
 
+/**
+ Starting with the following xml we want the format below, then back again
+
+ <root>
+    <FirstName>John</FirstName>
+    <LastName>Major</LastName>
+ </root>
+
+ To this
+
+ <root>
+    <row>
+        <Key>FirstName</Key>
+        <Value>John</Value>
+    </row>
+    <row>
+        <Key>LastName</Key>
+        <Value>Major</Value>
+    </row>
+ </root>
+ */
+
 def startingXmlTxt =
 '''<root>
     <FirstName>John</FirstName>
@@ -21,6 +43,18 @@ bldr.root {
             Key(aRt.name())
             Value(aRt.text())
         }
+    }
+}
+
+println XmlUtil.serialize( new XmlSlurper().parseText( sw.toString() ) )
+
+startingXml = new XmlSlurper().parseText(sw.toString())
+sw = new StringWriter()
+bldr = new MarkupBuilder(sw)
+
+bldr.root {
+    startingXml.'*'.each{ aRt ->
+        "${aRt.Key}"( aRt.Value )
     }
 }
 
