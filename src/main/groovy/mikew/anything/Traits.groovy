6c128b8b216392644d30ebbd237f6ebbeea65737
leadtrip <m1k3w000d@gmail.com>
2023-06-12 12:38:14 +0100

    Playing

--- src/main/groovy/mikew/anything/Traits.groovy
+++ src/main/groovy/mikew/anything/Traits.groovy
@@ -8,20 +8,32 @@ package mikew.anything
  */
 
 /**
- * Basic trait with 2 methods
+ * Basic trait with 2 concrete methods and one abstract
  */
 trait Switchable{
     void on(){
-        println "Brrrrr"
+        println "${name()} on"
+        running()
+    }
+
+    abstract void name()
+
+    void running() {
+        println "Hummmmmmmmmmm"
     }
 
     void off() {
-        println "Silence"
+        println "${name()} off"
     }
 }
 
 // Fridge class uses default Switchable implementation
-class Fridge implements Switchable{}
+class Fridge implements Switchable{
+    @Override
+    void name() {
+        println "Fridge"
+    }
+}
 
 def fridge = new Fridge()
 fridge.on()
@@ -30,8 +42,13 @@ fridge.off()
 // MassageGun overrides the on method from Switchable
 class MassageGun implements Switchable{
     @Override
-    void on() {
-        println "DDDDDDDDDDDDDDD"
+    void name() {
+        println "Massage gun"
+    }
+
+    @Override
+    void running() {
+        println "Buzzzzzzzzzz"
     }
 }
 
